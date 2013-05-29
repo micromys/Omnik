b1b3b38003283131a6c52e18c83393f11bf28151
micromys <info@micromys.nl>
2013-05-29 06:52:06 +0200

    Update to reflect latest changes
    Add sample code to on how to use inverterID

--- inverter_sample.php
+++ inverter_sample.php
@@ -13,8 +13,15 @@
 	$ip='192.168.1.2';		// your ip-address
 	$port=8899;			// do not change, unless you have other info about the port
 	$sn=602123456;			// your serial-number
+	$inverterID='';			// inverterID (optional if you do not know it leave empty otherwise 16 chars inverterID
+	//$inverterID='xxxxxxxxxxxxxxxx';	// inverterID (optional if do not know it leave empty
 
-	$inverter	=	new Inverter($ip,$port,$sn);		//	initialize class and create identification string
+	// if you provide an inverterID a check will take place if data returned is data from the inverter
+	// in case you more than one inverter
+	
+	$inverter	=	new Inverter($ip,$port,$sn);				//	initialize class and create identification string w/o inverterID
+	//$inverter	=	new Inverter($ip,$port,$sn,$inverterID);	//	initialize class and create identification string w/ inverterID
+
 	if ($inverter->read()===true)								//	read data from socket (inverter)
 	{
 		if($inverter->insert()===false)							//	store in db if true
